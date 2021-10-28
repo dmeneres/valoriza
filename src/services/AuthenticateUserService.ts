7a56152b3c77a595a726f7f6ffec4714613d22d9
Diogo Menéres <dmeneres@gmail.com>
2021-10-28 23:24:32 +0100

    final-aula-04

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getCustomRepository } from "typeorm"
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+import { UsersRepositories } from "../repositories/UserRepositories"
+
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({
+      email
+    });
+
+    if (!user) {
+      throw new Error ("Email or password is incorrect")
+    } else {
+      const passwordMatch = await compare(password, user.password)
+      if (!passwordMatch) {
+        throw new Error ("Email or password is incorrect")
+    } else {
+      const token = sign(
+        {
+        email: user.email
+        },
+        "4f93ac9d10cb751b8c9c646bc9dbccb9",
+        {
+        subject: user.id,
+        expiresIn: "1d"
+        }
+      );
+      return token
+    }
+
+    }
+  }
+}
+
+export { AuthenticateUserService }
